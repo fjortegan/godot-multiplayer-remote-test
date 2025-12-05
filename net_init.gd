46b2533e11fa9242e6d70915aefdde874ca6d182
fjortegan <fjortegan@gmail.com>
2025-12-05 14:11:03 +0100

    qwen UI initial

--- net_init.gd
+++ net_init.gd
@@ -1,17 +1,17 @@
 extends Node2D
 
-
-func _on_button_pressed() -> void:
+func _on_server_pressed() -> void:
 	Lobby.create_game()
-	$Button.disabled = true
-	$Button2.disabled = true
+	enable_buttons(false)
 
-
-func _on_button_2_pressed() -> void:
+func _on_client_pressed() -> void:
 	var peer = Lobby.join_game()
 	Lobby.player_connected.connect(_on_joined_game)
-	$Button.disabled = true
-	$Button2.disabled = true
+	enable_buttons(false)
+
+func enable_buttons(status=true):
+	$VBoxContainer/HBoxContainer4/ServerButton.disabled = status
+	$VBoxContainer/HBoxContainer4/ClientButton.disabled = status
 
 func _on_joined_game(peer_id, player_info):
 	Lobby.load_game("res://game.tscn")
