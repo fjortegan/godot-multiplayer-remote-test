4cdb57633bb782899dadaac81a9bda65667ce136
fjortegan <fjortegan@gmail.com>
2026-01-03 21:48:13 +0100

    lan lobby refactoring

--- scripts/game.gd
+++ scripts/game.gd
@@ -5,29 +5,27 @@ extends Node2D
 var pressed_buttons: int = 0
 
 func _ready():
-	# Preconfigure game.
-	if not multiplayer.is_server():
-		Global.current_lobby.player_loaded.rpc_id(1) # Tell the server that this peer has loaded.
-	else:
-		call_deferred("start_game")
+	call_deferred("start_game_server")
 
 func _input(event):
 	if event.is_action_pressed("ui_cancel"):
 		get_tree().quit()
 
 # Called only on the server.
-func start_game():
-	# All peers are ready to receive RPCs in this scene.
-	#Lobby.debug_log("game started: " + str(multiplayer.get_unique_id()))
-	var i = 0
-	for id in Global.current_lobby.players:
-		spawners[i%4].spawn_player(id, i)
-		i+=1
-	$Button1.activation.connect(_on_button_pressed)
-	$Button2.activation.connect(_on_button_pressed)
-	$Button3.activation.connect(_on_button_pressed)
-	$Button4.activation.connect(_on_button_pressed)
-	
+func start_game_server():
+	if multiplayer.is_server():
+		var i = 0
+		for id in Global.current_lobby.players:
+			spawners[i%4].spawn_player(id, i)
+			i+=1
+		$Button1.activation.connect(_on_button_pressed)
+		$Button2.activation.connect(_on_button_pressed)
+		$Button3.activation.connect(_on_button_pressed)
+		$Button4.activation.connect(_on_button_pressed)
+	else:
+		Global.current_lobby.player_loaded.rpc_id(1) # Tell the server that this peer has loaded.
+
+
 func _on_button_pressed(status: bool):
 	if status:
 		pressed_buttons += 1
