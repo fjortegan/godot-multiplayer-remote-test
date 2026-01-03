aca8f03ff8cd0370547433e75da20e5dd4745243
fjortegan <fjortegan@gmail.com>
2026-01-03 01:09:15 +0100

    steam lobby

--- player_info_list.gd
+++ player_info_list.gd
@@ -11,10 +11,11 @@ func _ready() -> void:
 	id_label.text = str(id)
 	name_label.text = player_info["name"]
 	var avatar_image
-	var avatar = player_info["avatar"]
-	if avatar is EncodedObjectAsID:
-		avatar =  load("res://images/characters/"+player_info["avatar_id"]+".png") as Avatar		
-		#avatar = instance_from_id(player_info["avatar"].get_object_id())
+	var avatar =  load("res://resources/"+player_info["avatar_id"]+".tres") as Avatar
+	#var avatar = player_info["avatar"]
+	#if avatar is EncodedObjectAsID:
+		#avatar =  load("res://resources/"+player_info["avatar_id"]+".tres") as Avatar		
+		##avatar = instance_from_id(player_info["avatar"].get_object_id())
 	avatar_image = avatar.image
 		
 	avatar_img.texture = avatar_image
