fac7687c89d73c3d144d44c0a52ffdce996ceedd
fjortegan <fjortegan@gmail.com>
2026-01-21 01:11:56 +0100

    bullets and health working

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -1,3 +1,4 @@
+class_name Bullet
 extends RigidBody2D
 
 const SPEED: float = 300
@@ -10,27 +11,17 @@ const SPEED: float = 300
 
 const DAMAGE: float = 10.0
 
-func _enter_tree() -> void:
-	Global.current_lobby.debug_log("enter bullet: %d %s %s" % [player_id, color, velocity])
-	set_multiplayer_authority(player_id)
+func _enter_tree() -> void: # server manages bullets
+	set_multiplayer_authority(1)
 
 func _ready() -> void:
+	if multiplayer.is_server(): # duration timer only on server
+		$Duration.start()
 	if not velocity == Vector2.ZERO:
 		linear_velocity = velocity.normalized() * SPEED
 	else:
 		linear_velocity = Vector2(0, SPEED)
 	sprite.modulate = color
-	#if not is_multiplayer_authority(): return
 
-func _on_bullet_timeout():
-	Global.current_lobby.debug_log("bullet timeout...")
-	dispose_bullet()
-
-@rpc("any_peer", "call_local", "reliable")
 func dispose_bullet():
-	Global.current_lobby.debug_log("dispose bullet...")
-	$MultiplayerSynchronizer.public_visibility = false
-	visible = false
-	set_physics_process(false)
-	await get_tree().create_timer(1).timeout
-	call_deferred("queue_free")
+	if is_multiplayer_authority(): call_deferred("queue_free")
--- scripts/player.gd
+++ scripts/player.gd
@@ -19,6 +19,8 @@ const MAXLIFE: float = 50.0
 const FONTSIZE = 12 
 const FONTCOLOR = Color.LAWN_GREEN 
 
+var _shooting: bool = false
+
 func _enter_tree() -> void:
 	set_multiplayer_authority(name.to_int())
 
@@ -32,8 +34,48 @@ func _ready() -> void:
 		nickname.add_theme_font_size_override("font_size", FONTSIZE)
 		nickname.add_theme_color_override("font_color", FONTCOLOR)
 
+# input: shoot
+func _input(event: InputEvent) -> void:
+	if not is_multiplayer_authority(): return # AUTHORITY ONLY
+	if event.is_action_pressed("ui_accept") and not _shooting:
+		shoot(velocity, Global.get_avatar().bullet_color, name.to_int())
+
+func _physics_process(_delta: float) -> void:
+	if is_multiplayer_authority(): # AUTHORITY ONLY
+		# manages movement, animation and name helper
+		# TODO: refactor
+		nickname.text = Global.get_player_name() + " %.0f PV" % current_life
+		if player_alive:
+			velocity = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down") * SPEED
+			if velocity == Vector2.ZERO:
+				animation.play_animation("idle", "south")
+			else:
+				animation.play_animation("walk", _direction_string(velocity))
+	move_and_slide()
+	if multiplayer.is_server(): # SERVER ONLY
+		# detect bullet collisions 
+		for i in get_slide_collision_count():
+			var collider = get_slide_collision(i).get_collider()
+			if collider.is_in_group("Bullet"):
+				receive_damage.rpc_id(name.to_int(), collider.DAMAGE)
+				collider.dispose_bullet()
+
+# order shoot on game (called locally)
+# TODO: create signal connected to Game
+func shoot(_velocity, _color, _player_id):
+	_shooting = true
+	$ShootCD.start()
+	# get Game root node
+	var root_node = get_parent().get_parent()
+	root_node.shoot(index, global_position, _velocity, Global.get_avatar().bullet_color)
+
+func _on_shoot_cd_timeout() -> void:
+	_shooting = false
+
 # receive damage and death
+@rpc("any_peer", "call_local", "reliable")
 func receive_damage(damage: float):
+	Global.current_lobby.debug_log("player %d receives %0.1f damage" % [index, damage])
 	current_life -= damage
 	if current_life <= 0:
 		current_life = 0
@@ -47,41 +89,8 @@ func _on_death():
 # when dead animation is finished, dispose this node
 func _on_animation_finished() -> void:
 	if not player_alive:
-		queue_free()
-		
-# input: shoot
-func _input(event: InputEvent) -> void:
-	if not is_multiplayer_authority(): return # AUTHORITY ONLY
-	if event.is_action_pressed("ui_accept"):
-		shoot(velocity, Global.get_avatar().bullet_color, name.to_int())
-
-# process: manages movement, animation and name helper 
-func _physics_process(_delta: float) -> void:
-	if not is_multiplayer_authority(): return # AUTHORITY ONLY
-	# TODO: refactor
-	nickname.text = Global.get_player_name() + " %.0f PV" % current_life
-	if player_alive:
-		velocity = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down") * SPEED
-		if velocity == Vector2.ZERO:
-			animation.play_animation("idle", "south")
-		else:
-			animation.play_animation("walk", _direction_string(velocity))
-	move_and_slide()
-	# detect bullet collisions
-	for i in get_slide_collision_count():
-		var collider = get_slide_collision(i).get_collider()
-		Global.current_lobby.debug_log("collision: " + name + " -> " + collider.name)
-		if collider.is_in_group("Bullet"):
-			#receive_damage(collider.DAMAGE)
-			collider.dispose_bullet.rpc_id(collider.player_id)
-
-# order shoot on game (called locally)
-# TODO: create signal connected to Game
-func shoot(_velocity, _color, _player_id):
-	# get Game root node
-	var root_node = get_parent().get_parent()
-	Global.current_lobby.debug_log("player shoot %d" % _player_id)
-	root_node.shoot(index, global_position, _velocity, Global.get_avatar().bullet_color)
+		if multiplayer.is_server():
+			queue_free()
 
 # calculates direction for animations
 func _direction_string(value: Vector2) -> String:
